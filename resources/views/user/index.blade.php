44b21338dfc09dbb6f00d574091c04a66ba39a46
Leandro <lfoadm@icloud.com>
2024-12-30 15:15:03 -0300

    video 03

---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+@section('content')
+<main class="pt-90">
+    <div class="mb-4 pb-4"></div>
+    <section class="my-account container">
+        <h2 class="page-title">Conta do usuário</h2>
+        <div class="row">
+            <div class="col-lg-3">
+                {{-- @include('user.account-nav') --}}
+            </div>
+            <div class="col-lg-9">
+                <div class="page-content my-account__dashboard">
+                    <p>Olá <strong>{{ Auth::user()->name }}</strong></p>
+                    <h1>
+                        INDEX DO USUÁRIO CONSUMIDOR....
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\User\UserController;
+use App\Http\Middleware\AuthAdmin;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -8,3 +11,15 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
+// #CONTA DO USUÁRIO FINAL (CONSUMIDOR)
+Route::middleware(['auth'])->group(function() {
+    Route::get('/account-dashboard', [UserController::class, 'index'])->name('user.index');
+    
+});
+
+// #CONTA DO ADMINISTRADOR
+Route::middleware(['auth', AuthAdmin::class])->group(function() {
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+    
+});
