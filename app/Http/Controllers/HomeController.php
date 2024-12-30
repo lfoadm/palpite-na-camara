f4c7fe0c71d2ee9fca84a07aae7d9dded721d3c9
Leandro <lfoadm@icloud.com>
2024-12-30 14:18:37 -0300

    video 02

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,23 +6,31 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
-        return view('home');
+        // $slides = Slide::where('status', 1)->get()->take(3);
+        // $orders = Order::orderBy('created_at', 'DESC')->get();
+        
+        // $orderItems = OrderItem::with(['candidate' => function($query) {
+        //     $query->where('elected', true);
+        // }])
+        // ->get()
+        // ->groupBy('candidate_id')
+        // ->map(function($group) {
+        //     return [
+        //         'count_elected' => $group->count(),
+        //         'candidate' => $group->first()->candidate,
+        //     ];
+        // });
+        
+        return view('index');
     }
+
+    // public function show($order_id)
+    // {
+    //     $order = Order::find($order_id);
+    //     $orderItems = OrderItem::where('order_id', $order->id)->orderBy('id')->paginate(13);
+    //     return view('show', compact('order', 'orderItems'));
+        
+    // }
 }
